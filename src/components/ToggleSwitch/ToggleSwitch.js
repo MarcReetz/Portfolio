918cb4d8cb37ae2cd9781911df24387c45e23996
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-06-29 15:16:26 +0200

    made changes to switch component

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import style from "./ToggleSwitch.module.css";
 
-export default function ToggleSwitch() {
-  const [isToggled, setIsToggled] = useState(false);
-  const onToggle = () => setIsToggled(!isToggled);
+export default function ToggleSwitch(props) {
   return (
     <label className={style.toggleSwitch}>
-      <input type="checkbox" checked={isToggled} onChange={onToggle} />
+      <input type="checkbox" checked={props.isGerman} onChange={props.onChange} />
       <span className={style.switch} />
     </label>
   );
