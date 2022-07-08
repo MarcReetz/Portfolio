493f98c2fc2f6dc6d0f99764caa84e93a0f4f170
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-07-09 00:30:31 +0200

    add about section and bug fixes in components

---
+++ src/pages/About/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+import style from "./About.module.css"
+import Title from "../../components/Title/Title"
+import Picture from "../../components/Picture/Picture"
+import { useTranslation } from "react-i18next";
+
+export default function About() {
+  const { t } = useTranslation();
+
+  return (
+    <section id="about">
+      <Title number="1" title={t("about_me")}/>
+      <div className={style.container}>
+        <div>
+          <p>
+            {t("about_me_text")}
+          </p>
+        </div>
+        <Picture src="pictures/me.jpeg" alt="Marc Reetz"/>
+      </div>
+    </section>
+  )
+} 
