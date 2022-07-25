32fc3657920abb84302b46ff9e94d3fa6fa9fe02
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-07-25 12:55:14 +0200

    Add Skill Site Component

--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -1,14 +1,19 @@
 import React from "react";
-import styles from "./Skills.module.css"
-import Title from "../../components/Title/Title"
+import styles from "./Skills.module.css";
+import Title from "../../components/Title/Title";
+import SkillWall from "../../components/SkillWall/SkillWall";
 import { useTranslation } from "react-i18next";
 
 export default function Skills() {
   const { t } = useTranslation();
 
   return (
-    <div className={styles.container}>
-      <Title number="2" title={t("skills")}/>
-    </div>
-  )
+    <section id="skills">
+      <Title number="2" title={t("skills")} />
+      <div className={styles.container}>
+        <p>{t("skills_text")}</p>
+        <SkillWall/>
+      </div>
+    </section>
+  );
 }
---
+++ src/pages/Skills/Skills.stories.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Skills from './Skills'
+
+export default {
+  title: 'Skills',
+  component: Skills
+};
+
+const Template = (args) => <Skills {...args}/>
+
+export const First = Template.bind({});
+
+First.args = {
+
+}
