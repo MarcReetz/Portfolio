b9f5fd1d02b322e629a5ae049a159d8fddefa44c
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-07-25 18:34:07 +0200

    add Contact Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
 import Hero from "./pages/Hero/Hero";
-import Skills from "./pages/Skills/Skills";
+//import Skills from "./pages/Skills/Skills";
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Hero/>
       <About/>
-      <Skills/>
+      <Contact/>
     </div>
   );
 }
