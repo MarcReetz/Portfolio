2f3137ef87209bcdb76dc25f1e77085257f6ca38
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-08-04 18:05:56 +0200

    Changes Nav Element

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import Nav from "./components/Nav/Nav";
 import NavMenu from "./components/Nav/NavMenu";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import Hero from "./pages/Hero/Hero";
+import { useTranslation } from "react-i18next";
 //import Skills from "./pages/Skills/Skills";
 
 function App() {
+  const { i18n } = useTranslation();
 
   const [navOn,setNavOn] = useState(false);
+
+  const changeLanguageHandler = (lang) => {
+      i18n.changeLanguage(lang);
+  };
+
+  useEffect(() => {
+
+  })
 
   const onClickNav = () => {
     setNavOn(!navOn);
   }
+
+
 
   return (
     <div className="container">
--- src/components/Nav/NavMenu.js
+++ src/components/Nav/NavMenu.js
@@ -1,31 +1,49 @@
 import { useTranslation } from "react-i18next";
 import styles from "./NavMenu.module.css";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 
-export default function NavMenu(prop) {
+export default function NavMenu(props) {
   const { t } = useTranslation();
 
   return (
     <div className={styles.container}>
-      <button className={styles.button} onClick={prop.onClick}>
+      <button className={styles.button} onClick={props.onClick}>
         <i className="fa-solid fa-x"></i>
       </button>
       <nav className={styles.navContainer}>
         <ol>
           <li>
-            <a href="#about" onClick={prop.onClick}>
+            <a href="#about" onClick={props.onClick}>
               {t("about")}
             </a>
           </li>
           <li>
-            <a href="#contact" onClick={prop.onClick}>
+            <a href="#contact" onClick={props.onClick}>
               {t("contact")}
             </a>
           </li>
         </ol>
       </nav>
-      <div className={styles.links}>
-        <a href="https://github.com/MarcReetz/" target="_blank" rel="noreferrer"><i class="fa-brands fa-github"></i></a>
-      </div>
+      <ul className={styles.links}>
+        <li>
+          <a
+            href="https://github.com/MarcReetz/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa-brands fa-github"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.xing.com/profile/Marc_Reetz/cv"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa-brands fa-square-xing"></i>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
