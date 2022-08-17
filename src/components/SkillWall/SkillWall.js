01551d3e357d88e57d30b982f389d03f06889d2d
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-08-17 23:47:53 +0200

    add Skillwall other relation

--- src/components/SkillWall/SkillWall.js
+++ src/components/SkillWall/SkillWall.js
@@ -1,42 +1,49 @@
 import React, { useState } from "react";
 import SkillIcon from "./SkillIcon";
-import SkillsJson from './../../../public/techRelations/techSkills.json';
+import SkillsJson from "./../../../public/techRelations/techSkills.json";
 
 export default function SkillWall() {
-
-  const [skills,setSkills] = useState(SkillsJson.icons);
+  const [skills, setSkills] = useState(SkillsJson.icons);
 
   const onClick = (skill) => {
-    let skillsC = skills.map(element => {
-      if(skill.name === element.name){
-        return {...element,relations:1}
-      }else{
-        return {...element,relations:0}
+    let skillsC = skills.map((element) => {
+      if (skill.name === element.name) {
+        return { ...element, relations: 1 };
+      } else {
+        return { ...element, relations: 0 };
       }
     });
 
-    relationsCheck(skill.name,2,skillsC);
+    relationsCheck(skill.name, 2, skillsC);
+    setSkills(skillsC);
+  };
 
-    setSkills(skillsC)
-  }
+  const relationsCheck = (name, level, skillsC) => {
+    let skill = skills.find((element) => element.name === name);
+    console.log("skill is " + skill);
+    if (skill) {
+      const otherRelations = skill.otherRelations;
 
-  const relationsCheck = (name,level,skillsC) => {
-    if(level >= 4) return;
-    for(let i = 0; i < skills.length; i++){
-      if(skills[i].childOff.includes(name)){
+      skillsC.forEach((element) => {
+        if (otherRelations.includes(element.name) && level === 2) {
+          element.relations = 3;
+          console.log("has been hid ")
+        }
+      });
+    }
+
+    if (level >= 4) return;
+    for (let i = 0; i < skills.length; i++) {
+      if (skills[i].childOff.includes(name)) {
         skillsC[i].relations = level;
-        relationsCheck(skills[i].name,level+1,skillsC);
+        relationsCheck(skills[i].name, level + 1, skillsC);
       }
     }
-  }
+  };
 
-  let SkillIcons = skills.map(element => {
-    return <SkillIcon icon={element} onClick={onClick} key={element.name}/>
+  let SkillIcons = skills.map((element) => {
+    return <SkillIcon icon={element} onClick={onClick} key={element.name} />;
   });
 
-  return (
-    <>
-      {SkillIcons}
-    </>
-  )
+  return <>{SkillIcons}</>;
 }
