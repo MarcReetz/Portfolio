d2ab2f0fe1f8b45e280f948aa3996f37beeb3e56
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-10-11 12:51:39 +0200

    Imprint German and Privacy German

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState,useEffect } from "react";
+import { useState} from "react";
 import Nav from "./components/Nav/Nav";
 import NavMenu from "./components/Nav/NavMenu";
 import About from "./pages/About/About";
@@ -8,14 +8,11 @@ import { useTranslation } from "react-i18next";
 import Projects from "./pages/Projects/Projects";
 import Skills from "./pages/Skills/Skills"
 import Footer from "./components/Footer/Footer";
-import Legal from "./components/Legal/Legal";
-//import Skills from "./pages/Skills/Skills";
 
 function App() {
   const { i18n } = useTranslation();
 
   const [navOn,setNavOn] = useState(false);
-  const [legalOn,setLegalOn] = useState(0);
 
   const changeLanguageHandler = (lang) => {
       i18n.changeLanguage(lang);
@@ -25,29 +22,16 @@ function App() {
     setNavOn(!navOn);
   }
 
-  const onClickLegal = () => {
-    setLegalOn(0)
-  }
-
-  const onClickImprint = () => {
-    setLegalOn(1);
-  }
-
-  const onClickPrivacy = () => {
-    setLegalOn(2)
-  }
-
   return (
     <div className="container">
       <Nav onClick={onClickNav} isVisible={!navOn}/>
       { navOn && <NavMenu onClick={onClickNav} changeLanguageHandler={changeLanguageHandler}/>}
-      { legalOn > 0 && <Legal onClick={onClickLegal} legalOn={legalOn}/>}
       <Hero/>
       <About/>
       <Skills />
       <Projects />
       <Contact/>
-      <Footer onClickImprint={onClickImprint} onClickPrivacy={onClickPrivacy}/>
+      <Footer/>
     </div>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,14 +2,14 @@ import LinkButton from "../Button/LinkeButton";
 import { useTranslation } from "react-i18next";
 import styles from './Footer.module.css'
 
-export default function Footer (props) {
+export default function Footer () {
   const {t} = useTranslation();
 
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <LinkButton text={t("imprint")} onClick={props.onClickImprint}/>
-        <LinkButton text={t("privacy")} onClick={props.onClickPrivacy}/>
+        <LinkButton text={t("imprint")} />
+        <LinkButton text={t("privacy")} />
       </div>
       { }
     </footer>
