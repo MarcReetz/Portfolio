dfb828e0b6dcb366d85c13fc69e6493cb27e3da6
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-10-13 22:53:18 +0200

    add Language change info

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import styles from "./Nav.module.css"
 
 export default function Nav(prop) {
+  const {t} = useTranslation();
   return (
     <header className={styles.header}>
       {prop.isVisible && <button className={styles.button} onClick={prop.onClick}><i class="fa-solid fa-bars"></i></button>}
+      <div className={styles.languageInfo}>
+        <p>{t("languageInfo")}</p>
+        <i class="fa-solid fa-turn-up"></i>
+      </div>
     </header>
   )
 }
