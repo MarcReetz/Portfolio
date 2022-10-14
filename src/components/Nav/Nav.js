045e7a99f64d25708eba8e7e7d82b6a295b79096
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-10-14 15:24:01 +0200

    Add event listener info test language

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,16 +1,41 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import styles from "./Nav.module.css"
+import styles from "./Nav.module.css";
+import { useEffect,useState } from "react";
 
 export default function Nav(prop) {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
+
+  const [languageInfoVisible, setLanguageInfoVisible] = useState(true);
+  
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    if(position !== 0){
+      setLanguageInfoVisible(false)
+    }else{
+      setLanguageInfoVisible(true)
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <header className={styles.header}>
-      {prop.isVisible && <button className={styles.button} onClick={prop.onClick}><i class="fa-solid fa-bars"></i></button>}
-      <div className={styles.languageInfo}>
+      {prop.isVisible && (
+        <button className={styles.button} onClick={prop.onClick}>
+          <i class="fa-solid fa-bars"></i>
+        </button>
+      )}
+      {languageInfoVisible && <div className={styles.languageInfo}>
         <p>{t("languageInfo")}</p>
         <i class="fa-solid fa-turn-up"></i>
-      </div>
+      </div>}
     </header>
-  )
+  );
 }
