9df10afb67a0e33456d21987b3d6a50f9b8481c4
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-10-18 13:50:49 +0200

    add height difference and auto resize

--- src/components/SkillArea/SkillArea.js
+++ src/components/SkillArea/SkillArea.js
@@ -2,22 +2,39 @@ import { Stage, Text } from "react-pixi-fiber";
 import { useEffect, useState } from "react";
 import Line from "./PixiLine";
 
-const width = 600;
-const height = 400;
-const options = {
-  antialias: true,
-  backgroundColor: 0x56789a,
-  width: width,
-  height: height,
-};
-
-const style = {
-  width: width,
-  height: height,
-};
-
 export default function SkillArea() {
   const [skills, setSkills] = useState([]);
+
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+  useEffect(() => {
+    function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
+  const width = dimensions.width;
+  const height = 400;
+  const options = {
+    antialias: true,
+    backgroundColor: 0x56789a,
+    width: width,
+    height: height,
+  };
+
+  const style = {
+    width: width,
+    height: height,
+  };
 
   useEffect(() => {
     fetch("/data/techRelations/techSkillsArea.json")
@@ -34,9 +51,12 @@ export default function SkillArea() {
   }, []);
 
   const Texts = skills.map((skill) => {
+    const x = (skill.fbFactor / 100) * width;
+    let y = height / 2;
 
-    const x = skill.fbFactor/100 * width
-    const y = height/2
+    if (skill.heightDiv) {
+      y = height / 2 + (height / 100) * skill.heightDiv;
+    }
 
     return (
       <Text
