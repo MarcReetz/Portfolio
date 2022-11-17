abce579329be2b3f60ed3d6ed509b288d8984dcc
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-11-17 02:27:07 +0100

    add translation to project wall

--- src/components/ProjectWall/ProjectWall.js
+++ src/components/ProjectWall/ProjectWall.js
@@ -1,13 +1,21 @@
 import ProjectElement from './ProjectElement'
 import styles from './ProjectWall.module.css'
 import { useEffect, useState } from 'react'
+import { useTranslation } from "react-i18next";
 
 export default function ProjectWall () {
 
   const [projectsList,setProjectsList] = useState([]);
+  const { i18n } = useTranslation()
 
   useEffect( () => {
-    fetch( "/data/projects/projects.json")
+    let url
+    if(i18n.language === 'de'){
+      url = ("/data/projects/de/projects.json")
+    }else {
+      url = ("/data/projects/projects.json")
+    }
+    fetch(url)
     .then(function (res) {
       return res.json();
     })
@@ -19,7 +27,7 @@ export default function ProjectWall () {
     .catch(function (err) {
       console.log(err, " error");
     })
-  },[])
+  },[i18n.language])
 
   let projects = projectsList.length > 0 && projectsList.map(element => {
     return <ProjectElement title={element.name} text={element.text} link={element.link} gitLink={element.gitLink} techs={element.techs} key={element.name}/>
