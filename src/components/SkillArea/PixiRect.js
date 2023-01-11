a357f78d7d1005c7a671a584ebaf7666ba405fa5
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2023-01-11 16:08:50 +0100

    disable eslint for one line, Pixi themes to actually use variable

--- src/components/SkillArea/PixiRect.js
+++ src/components/SkillArea/PixiRect.js
@@ -6,6 +6,8 @@ export const behavior = {
   customDisplayObject: () => new PIXI.Graphics(),
   customApplyProps: function (instance, oldProps, newProps) {
     const { fill, x, y, width, height, ...newPropsRest } = newProps;
+    // variables actually theme to be used by PIXI if the screen size changes
+    // eslint-disable-next-line no-unused-vars
     const { fill: oldFill, x: oldX, y: oldY, width: oldWidth, height: oldHeight, ...oldPropsRest } = oldProps || {};
     if (typeof oldProps !== "undefined") {
       instance.clear();
