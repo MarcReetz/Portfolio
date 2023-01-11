1adaec31462f5d77af25f9e3c03c48eea6597091
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2023-01-11 15:52:16 +0100

    fix resize bug

--- src/components/SkillArea/PixiRect.js
+++ src/components/SkillArea/PixiRect.js
@@ -6,7 +6,7 @@ export const behavior = {
   customDisplayObject: () => new PIXI.Graphics(),
   customApplyProps: function (instance, oldProps, newProps) {
     const { fill, x, y, width, height, ...newPropsRest } = newProps;
-    const { ...oldPropsRest } = oldProps || {};
+    const { fill: oldFill, x: oldX, y: oldY, width: oldWidth, height: oldHeight, ...oldPropsRest } = oldProps || {};
     if (typeof oldProps !== "undefined") {
       instance.clear();
     }
